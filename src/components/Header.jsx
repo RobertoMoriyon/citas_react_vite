38637424527c1dae6ed39a93f3b9114e4f4004ba
ROBERTO MORIYON VILLA <roberto.moriyon@es.ibm.com>
2022-10-18 13:04:31 +0200

    Actualización del Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
 
     return (
-        <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">Seguimientos Pacienes {''}
+        <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">Seguimientos Pacienes {''} con GitHub {''}
             <span className="text-indigo-600">Veterinaria</span>
         </h1>
     )
